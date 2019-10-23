dd4b7cb5fac023205914d6cf84d34b3df9eab5ba
ashirko <antonshirko@frtk.ru>
2019-10-23 11:40:02 +0300

    Print info about multiple instances

--- start_monitor.py
+++ start_monitor.py
@@ -3,10 +3,23 @@
 import argparse
 from monitor import TcpmirrorMonitor
 from config import Config
+import os
+
+
+def print_info(filename):
+    config = Config(filename)
+    monitor = TcpmirrorMonitor(config)
+    monitor.start()
+
 
 parser = argparse.ArgumentParser()
-parser.add_argument("path", help="a path to a file of files")
+parser.add_argument("path", help="a file of a path to files")
 args = parser.parse_args()
-config = Config(args.path)
-monitor = TcpmirrorMonitor(config)
-monitor.start()
+if os.path.isdir(args.path):
+    for file in os.listdir(args.path):
+        if file.endswith(".toml"):
+            print_info(file)
+elif os.path.isfile(args.path) and args.path.endswith(".toml"):
+    print_info(args.path)
+else:
+    print("ERROR: invalid file or path to a files")
